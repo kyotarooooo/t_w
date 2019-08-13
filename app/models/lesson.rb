2c381b7f66fdda67df7f79613d00bdbdac15ae6d
kyotarooooo <kentaro.inoue.1121@gmail.com>
2019-08-13 13:33:02 +0000

    レイアウトを整えた

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,5 @@
 class Lesson < ApplicationRecord
 	belongs_to :user
-	has_many :attendances
+	has_many :attendances, dependent: :destroy
 	has_many :attendanced_users, through: :attendances, source: :user
 end
